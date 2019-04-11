4f4a8db97a8c2795c1db6d8b59962189b769aa30
zcribe <erlend.eelmets@gmail.com>
2019-04-11 14:05:34 +0300

    Minimal form, blackbox form test

--- LinkGenerator/views.py
+++ LinkGenerator/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index_view(request):
-    return HttpResponse("<html><title>URLShortener</title></html>")
+    return render(request, 'LinkGenerator/index.html')
--- functional_tests.py
+++ functional_tests.py
@@ -1,30 +1,61 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
 
 
-# Joe starts hit browser
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
 
-# He needs to dictate the link to a news story over the phone. He googles
-# for a link shortener and navigates to the page.
-browser.get('http://localhost:8000')
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
 
-# He notices that the page title and header mention that it is a URL
-# shortening service.
-assert 'URLShortener' in browser.title
+    def tearDown(self) -> None:
+        self.browser.quit()
 
-# He sees a form there.
+    def test_can_generate_a_new_short_url(self) -> None:
+        # He needs to dictate the link to a news story over the phone. He googles
+        # for a link shortener and navigates to the page.
+        self.browser.get('http://localhost:8000')
 
-# He is invited to paste in a link and click a button
+        # He notices that the page title and header mention that it is a URL
+        # shortening service.
+        self.assertIn('URLShortener', self.browser.title)
 
-# He pastes in a link and clicks on a button
+        # He sees a form there
 
-# He is redirected to a new page.
+        form = self.browser.find_element_by_tag_name('form').tag_name
 
-# He sees the title of the page
+        self.assertEqual(form, 'form')
 
-# He notices a generated link
+        # He sees an input there and its label
 
-# He copies it and then visits it to check it.
+        input_field = self.browser.find_element_by_tag_name('input').tag_name
+        input_field_id = self.browser.find_element_by_id('link-generator-input').text
+        input_field_label = self.browser.find_element_by_tag_name('label').tag_name
 
-# He is happy with the service provided and closes the browser.
-browser.close()
+        self.assertEqual(input_field, 'input')
+        self.assertEqual(input_field_id, 'link-generator-input')
+        self.assertEqual(input_field_label, 'label')
+
+        # He sees a submit button there
+
+        button = self.browser.find_element_by_tag_name('button').tag_name
+        button_content = self.browser.find_element_by_tag_name('button').text
+
+        self.assertEqual(button, 'button')
+        self.assertEqual(button_content, 'Submit')
+
+        # He is invited to paste in a link and click a button
+        self.fail()
+
+        # He pastes in a link and clicks on a button
+
+        # He is redirected to a new page.
+
+        # He sees the title of the page
+
+        # He notices a generated link
+
+        # He copies it and then visits it to check it.
+
+        # He is happy with the service provided and closes the browser.
