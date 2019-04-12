388b5bd2cc68899f81baf1c66dd42509a9efa694
zcribe <erlend.eelmets@gmail.com>
2019-04-12 12:55:08 +0300

    View changes, mock form replaced with real form, view tests added.

--- LinkGenerator/views.py
+++ LinkGenerator/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render, HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import LinkGeneratorForm
 
 
 # Create your views here.
 def index_view(request):
-    return render(request, 'LinkGenerator/index.html')
+    if request.method == 'POST':
+        form = LinkGeneratorForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect('link')
+
+    form = LinkGeneratorForm()
+    return render(request, 'LinkGenerator/index.html', {'form': form})
+
+
+def response_view(request):
+    return HttpResponse('Works')
