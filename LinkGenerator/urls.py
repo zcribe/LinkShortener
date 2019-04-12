cab2e6bed8b4b55b627c255677bd45f94d8f5bf2
zcribe <erlend.eelmets@gmail.com>
2019-04-12 17:04:29 +0300

    Functional

--- LinkGenerator/urls.py
+++ LinkGenerator/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import index_view, response_view
+from .views import index_view, response_view, redirect_view
 
 urls = [
     path('', index_view),
-    path('link', response_view)
+    path('link', response_view),
+    path('re/<slug:slug>', redirect_view)
 ]
--- LinkGenerator/views.py
+++ LinkGenerator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from django.http import HttpResponseRedirect
+from django.views.decorators.http import require_POST
 from .forms import LinkGeneratorForm
 from .models import RedirectorModel
 from .helper import random_slugger
@@ -14,12 +15,21 @@ def index_view(request):
             lil_slug = random_slugger()
             new = RedirectorModel(original_link=original_link, id_slug=lil_slug)
             new.save()
-            return HttpResponseRedirect('link', {'original_link': original_link,
-                                                 'lil_slug': lil_slug})
+            request.session['original_link'] = original_link
+            request.session['lil_slug'] = lil_slug
+            return HttpResponseRedirect('link')
 
     form = LinkGeneratorForm()
     return render(request, 'LinkGenerator/index.html', {'form': form})
 
 
 def response_view(request):
-    return render(request, 'LinkGenerator/response.html')
+    originl_link = request.session.get('original_link')
+    lil_slug = request.session.get('lil_slug')
+    return render(request, 'LinkGenerator/response.html', {'original_link': originl_link,
+                                                           'lil_slug': lil_slug})
+
+
+def redirect_view(request, slug):
+    redir = RedirectorModel.objects.filter(id_slug=slug).get()
+    return HttpResponseRedirect(redir.original_link)
