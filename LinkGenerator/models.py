a24b0d3bd5e8fd0caf3a157b67286445c4273875
zcribe <erlend.eelmets@gmail.com>
2019-04-12 12:55:48 +0300

    Redirector model added, link url, helpers file added

--- LinkGenerator/models.py
+++ LinkGenerator/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class RedirectorModel(models.Model):
+    original_link = models.URLField()
+    id_slug = models.SlugField
--- LinkGenerator/urls.py
+++ LinkGenerator/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import index_view
+from .views import index_view, response_view
 
 urls = [
-    path('', index_view)
+    path('', index_view),
+    path('link', response_view)
 ]
